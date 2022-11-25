8d96b5690b85b205a26e2d1852a24d4af4846157
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-25 00:10:26 -0400

    feat: add getAll method to fake productRepo

--- tests/fakes/product_repository_fake.ts
+++ tests/fakes/product_repository_fake.ts
@@ -4,10 +4,14 @@ import {
   AddProductOutput,
 } from "@/add_product";
 
-export class productRepositoryFake implements IAddProductRepository {
+import { IGetProductsRepository, GetProductsOutput } from "@/get_products";
+
+export class productRepositoryFake
+  implements IAddProductRepository, IGetProductsRepository
+{
   private _datas: Array<AddProductOutput> = [];
 
-  add(input: AddProductInput): AddProductOutput {
+  public add(input: AddProductInput): AddProductOutput {
     const product = Object.assign({
       ...input,
       id: `product_${this._datas.length + 1}`,
@@ -16,5 +20,9 @@ export class productRepositoryFake implements IAddProductRepository {
     this._datas.push(product);
 
     return product;
+  }
+
+  public getAll(): GetProductsOutput[] {
+    return this._datas;
   }
 }
