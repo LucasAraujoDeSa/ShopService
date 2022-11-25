29301024808d287df66847c96cf8e8f87462b65d
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-24 23:12:44 -0400

    test: add fake ProductRepo

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -1,13 +1,16 @@
 import { AddProducts } from "../src/add_product";
+import { productRepositoryFake } from "./fakes/product_repository_fake";
 
 test("Should add products", () => {
+  const repo = new productRepositoryFake();
+
   const input = {
     name: "notebook",
     price: 2.0,
     quantity: 30,
   };
 
-  const product = AddProducts.add(input);
+  const product = AddProducts.add(input, repo);
 
   expect(product.quantity).toBe(30);
 });
