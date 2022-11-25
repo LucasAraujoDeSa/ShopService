d6a2a39f70ae50e2b5e7f864a4d37f2b0585c8ff
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-24 23:55:12 -0400

    feat: add addProduct Output type

--- src/add_product.ts
+++ src/add_product.ts
@@ -4,25 +4,27 @@ export type AddProductInput = {
   quantity: number;
 };
 
+export type AddProductOutput = {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+};
+
 export interface IProductRepository {
-  add: (input: AddProductInput) => void;
+  add: (input: AddProductInput) => AddProductOutput;
 }
 
 export class AddProducts {
   constructor(private productRepository: IProductRepository) {}
 
-  public add({ name, price, quantity }: AddProductInput) {
-    this.productRepository.add({
+  public add({ name, price, quantity }: AddProductInput): AddProductOutput {
+    const product = this.productRepository.add({
       name,
       price,
       quantity,
     });
 
-    return {
-      id: "productId",
-      name: name,
-      price: price,
-      quantity: quantity,
-    };
+    return product;
   }
 }
--- tests/fakes/product_repository_fake.ts
+++ tests/fakes/product_repository_fake.ts
@@ -1,9 +1,20 @@
-import { IProductRepository, AddProductInput } from "@/add_product";
+import {
+  IProductRepository,
+  AddProductInput,
+  AddProductOutput,
+} from "@/add_product";
 
 export class productRepositoryFake implements IProductRepository {
-  private _datas: any = [];
+  private _datas: Array<AddProductOutput> = [];
 
-  add(input: AddProductInput) {
-    this._datas.push(input);
+  add(input: AddProductInput): AddProductOutput {
+    const product = Object.assign({
+      ...input,
+      id: `product_${this._datas.length + 1}`,
+    });
+
+    this._datas.push(product);
+
+    return product;
   }
 }
