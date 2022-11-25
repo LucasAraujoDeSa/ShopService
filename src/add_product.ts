4f246c870d8cec1fcdbcdd1bb7f502ec9a2ccd38
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-24 23:11:10 -0400

    feat: add productRepo to addProduct

--- src/add_product.ts
+++ src/add_product.ts
@@ -1,11 +1,24 @@
-type IAddProductInput = {
+export type AddProductInput = {
   name: string;
   price: number;
   quantity: number;
 };
 
+export interface IProductRepository {
+  add: (input: AddProductInput) => void;
+}
+
 export const AddProducts = {
-  add({ name, price, quantity }: IAddProductInput) {
+  add(
+    { name, price, quantity }: AddProductInput,
+    productRepository: IProductRepository
+  ) {
+    productRepository.add({
+      name,
+      price,
+      quantity,
+    });
+
     return {
       id: "productId",
       name: name,
