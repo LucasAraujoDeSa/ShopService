0d1e83fb8e12122ea19e10cb38daf46883428e21
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-26 13:15:46 -0400

    test: make get_product

---
+++ tests/get_product.test.ts
@@ -0,0 +1,35 @@
+import { GetProduct, AddProducts } from "@/index";
+import { productRepositoryFake } from "@/tests/fakes/product_repository_fake";
+
+const makeSut = () => {
+  const productRepo = new productRepositoryFake();
+  const addProducts = new AddProducts(productRepo);
+  const sut = new GetProduct(productRepo);
+  return {
+    sut,
+    addProducts,
+    productRepo,
+  };
+};
+
+const mock = () => {
+  return {
+    name: "notebook",
+    price: 2.0,
+    quantity: 30,
+  };
+};
+
+describe("GetProduct", () => {
+  test("Should return a product", () => {
+    const { addProducts, sut } = makeSut();
+
+    const input = mock();
+
+    const newProduct = addProducts.execute(input);
+
+    const product = sut.execute(newProduct.id);
+
+    expect(product?.name).toBe("notebook");
+  });
+});
