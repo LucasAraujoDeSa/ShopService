86d6f8c94068570690d1ad0d082572f56be96576
LucasAraujoDeSa <andrelucassaaraujo@gmail.com>
2022-11-27 17:35:27 -0400

    feat: add services to app.module

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
-import { ProductController } from "./controllers/product.controller";
+import { ProductController } from "./products/product.controller";
+import { AddProductService } from "./products/add_product.service";
+import { GetProductService } from "./products/get_product.service";
+import { GetProductsService } from "./products/get_products.service";
 
 @Module({
   controllers: [ProductController],
+  providers: [AddProductService, GetProductService, GetProductsService],
 })
 export class AppModule {}
